4ea45983c8b5b9b7fa30a920a041444726e5e776
Izumi <1042631369@qq.com>
2021-09-06 21:24:55 +0800

    init

---
+++ src/main/java/thoughtworks/rachael/zuul/RachaelZuulApplication.java
@@ -0,0 +1,11 @@
+package thoughtworks.rachael.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachaelZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RachaelZuulApplication.class, args);
+    }
+}
