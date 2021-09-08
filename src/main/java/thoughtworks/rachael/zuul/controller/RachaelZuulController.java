927ac5d4466e1460cb1814a65ebc79846aba67bc
Izumi <1042631369@qq.com>
2021-09-08 12:55:54 +0800

    move config to nacos config center

--- src/main/java/thoughtworks/rachael/zuul/controller/RachaelZuulController.java
+++ src/main/java/thoughtworks/rachael/zuul/controller/RachaelZuulController.java
@@ -1,13 +1,24 @@
 package thoughtworks.rachael.zuul.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class RachaelZuulController {
+
+    @Value("${test.property}")
+    private String test_property;
 
     @GetMapping("/")
     public String index() {
         return "welcome to Rachael Zuul mirco server";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return test_property;
     }
 }
