798d3ec27ae29446cb917e35ba8ed6b86d3d11f6
Izumi <1042631369@qq.com>
2021-09-08 14:53:38 +0800

    update deploy-local.sh

--- ci/deploy-local.sh
+++ ci/deploy-local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 _project_name=$1
 _project_version=$2
@@ -8,9 +8,13 @@ imageName=$_project_name:$_project_version
 
 echo "imageName ====> $imageName"
 
-containerId=`docker ps -a | grep -w $_project_name:$_project_version | awk '{print $1}'`
+containerId=`docker ps -a | grep -w ${_project_name}:${_project_version} | awk '{print $1}'`
+containerId_2=`docker ps -a | grep -w $_project_name:$_project_version | awk '{print $1}'`
+containerId_3=`docker ps -a | grep -w $_project_name:$_project_version | awk '{print $2}'`
 
 echo "containerId ====> $containerId"
+echo "containerId_2 ====> $containerId_2"
+echo "containerId_3 ====> $containerId_3"
 
 if [ "${containerId}" != "" ] ; then
 	docker stop $containerId
